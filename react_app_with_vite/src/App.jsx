30493334552af21b63c79d71c38144164c5e48a0
Akash Patil <patilakash211@gmail.com>
2023-12-21 22:58:53 +0530

    Counter with usestate

---
+++ react_app_with_vite/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+function App() {
+  const [counter, setCounter] = useState(0);
+  // let counter = 5;
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+    <h1>Hello Akash</h1>
+    <h2>Counter value {counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <br />
+    <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
