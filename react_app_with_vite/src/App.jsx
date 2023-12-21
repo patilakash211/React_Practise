3ff73c4106962f9b87b6aaacdab5a0143d9e2eff
Akash Patil <patilakash211@gmail.com>
2023-12-21 23:23:58 +0530

    Updating counter with conditions

--- react_app_with_vite/src/App.jsx
+++ react_app_with_vite/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
-  // let counter = 5;
 
-  const addValue = () =>{
-    setCounter(counter + 1)
-  }
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
 
-  const removeValue = () =>{
-    setCounter(counter - 1)
-  }
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
-    <h1>Hello Akash</h1>
-    <h2>Counter value {counter}</h2>
-    <button onClick={addValue}>Add Value</button>
-    <br />
-    <button onClick={removeValue}>Remove value</button>
+      <h1>Hello Akash</h1>
+      <h2>Counter value {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
